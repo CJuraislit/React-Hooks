449e25ab823c400b0fa98abaff6ec0d4d870281d
CJ laptop <korolevpavel02@gmial.com>
2024-04-23 18:26:48 +0300

    useDebounce hook

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,30 @@ import "./App.css";
 import useInput from "./hooks/useInput";
 import Hover from "./components/Hover";
 import useHover from "./hooks/useHover";
-import { useRef } from "react";
+import { useState } from "react";
 import List from "./components/List";
+import useDebounce from "./hooks/useDebounce";
 
 function App() {
-  const ref = useRef();
-  const isBlackHovering = useHover(ref);
+  const [value, setValue] = useState("");
+  const debauncedSearch = useDebounce(search, 500);
+  function search(query) {
+    fetch(`https://jsonplaceholder.typicode.com/todos?query=` + query)
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+      });
+  }
+
+  const onChange = (e) => {
+    setValue(e.target.value);
+    debauncedSearch(e.target.value);
+  };
 
   return (
-    <>
-      <List />
-    </>
+    <div>
+      <input type="text" value={value} onChange={onChange} />
+    </div>
   );
 }
 
